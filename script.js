9a41e68c38fdb0123faa1d667aa02d5fbad69e2d
Ilyes Houha <mi.houha@gmail.com>
2020-12-06 18:10:55 +0100

    Animate Menu Items

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const nav1 = document.getElementById("nav-1");
 const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
-const nav5 = document.getElementById("nav-5 ");
+const nav5 = document.getElementById("nav-5");
 
 function toggleNav() {
   // Toggle: Menu Bars Open/closed
@@ -14,8 +14,28 @@ function toggleNav() {
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
     overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    nav1.classList.remove("slide-out-1");
+    nav1.classList.add("slide-in-1");
+    nav2.classList.remove("slide-out-2");
+    nav2.classList.add("slide-in-2");
+    nav3.classList.remove("slide-out-3");
+    nav3.classList.add("slide-in-3");
+    nav4.classList.remove("slide-out-4");
+    nav4.classList.add("slide-in-4");
+    nav5.classList.remove("slide-out-5");
+    nav5.classList.add("slide-in-5");
   } else {
     overlay.classList.add("overlay-slide-right", "overlay-slide-left");
+    nav1.classList.remove("slide-in-1");
+    nav1.classList.add("slide-out-1");
+    nav2.classList.remove("slide-in-2");
+    nav2.classList.add("slide-out-2");
+    nav3.classList.remove("slide-in-3");
+    nav3.classList.add("slide-out-3");
+    nav4.classList.remove("slide-in-4");
+    nav4.classList.add("slide-out-4");
+    nav5.classList.remove("slide-in-5");
+    nav5.classList.add("slide-out-5");
   }
 }
 
